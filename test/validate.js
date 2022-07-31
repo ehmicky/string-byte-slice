1647570407167111b9ac807fa025ebeb0c1af4e7
ehmicky <ehmicky@gmail.com>
2022-07-31 17:57:25 +0200

    Add more tests

---
+++ test/validate.js
@@ -0,0 +1,26 @@
+import test from 'ava'
+import stringByteSlice from 'string-byte-slice'
+import { each } from 'test-each'
+
+each(
+  [
+    [],
+    [true],
+    [''],
+    ['', true],
+    // eslint-disable-next-line no-magic-numbers
+    ['', 0.5],
+    ['', '1'],
+    ['', Number.NaN],
+    ['', 0, true],
+    // eslint-disable-next-line no-magic-numbers
+    ['', 0, 0.5],
+    ['', 0, '1'],
+    ['', 0, Number.NaN],
+  ],
+  ({ title }, args) => {
+    test(`Arguments are validated | ${title}`, (t) => {
+      t.throws(stringByteSlice.bind(undefined, ...args))
+    })
+  },
+)
