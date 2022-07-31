93a7c3b93e6774a2ed1c0bcd23b03b921ab40c49
ehmicky <ehmicky@gmail.com>
2022-07-31 19:33:18 +0200

    Improve tests

--- test/main.js
+++ test/main.js
@@ -5,26 +5,34 @@ import { each } from 'test-each'
 each(
   /* eslint-disable no-magic-numbers */
   [
+    // Normal positive indices
+    ['abcd', 'bcd', 1],
+    ['abc', 'abc', 0, 3],
+
+    // Mix of positive|negative indices
     ['abcd', 'bc', 1, 3],
     ['abcd', 'bc', 1, -1],
     ['abcd', 'bc', -3, 3],
     ['abcd', 'bc', -3, -1],
-    ['abcd', 'bcd', 1],
+
+    // Very low|high indices
     ['abcd', '', 10],
     ['abcd', '', 0, -10],
     ['abcd', 'abcd', -10],
     ['abcd', 'abcd', 0, 10],
+
+    // Empty string
     ['', '', 0],
-    ['\u00B1bc\u00B1', '\u00B1bc\u00B1', 0],
-    ['\u00B1bc\u00B1', '\u00B1bc', 0, 5],
-    ['\u00B1bc\u00B1', 'bc', 1, 5],
-    ['\u00B1bc\u00B1', 'bc', 2, 5],
-    ['\u00B1bc\u00B1', 'c', 3, 5],
-    ['\u00B1bc\u00B1', '', 4, 5],
-    ['\u00B1bc\u00B1', '\u00B1bc', 0, 4],
-    ['\u00B1bc\u00B1', '\u00B1b', 0, 3],
-    ['\u00B1bc\u00B1', '\u00B1', 0, 2],
-    ['\u00B1bc\u00B1', '', 0, 1],
+
+    // 2-bytes characters
+    ['\u00B1a', '\u00B1a', 0],
+    ['\u00B1a', 'a', 1],
+    ['\u00B1a', 'a', 2],
+    ['\u00B1a', '', 3],
+    ['a\u00B1', 'a\u00B1', 0, 3],
+    ['a\u00B1', 'a', 0, 2],
+    ['a\u00B1', 'a', 0, 1],
+    ['a\u00B1', '', 0, 0],
   ],
   /* eslint-enable no-magic-numbers */
   ({ title }, [input, output, byteStart, byteEnd]) => {
