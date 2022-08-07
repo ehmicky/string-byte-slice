f32a64a09e31894ed04793ca14bb88931d00fdb8
ehmicky <ehmicky@gmail.com>
2022-08-07 23:29:21 +0200

    Move file

---
+++ src/char_code.js
@@ -0,0 +1,22 @@
+import { byteToChar } from './direction.js'
+import { replaceInvalidSurrogate } from './surrogate.js'
+
+// Variant that slices by iterating over the string using `String.charCode()`
+export const charCodeSlice = function (string, byteStart, byteEnd) {
+  const charStart = byteToChar(string, byteStart, false)
+  const charEnd = getByteEnd(string, byteEnd)
+  const stringA =
+    charStart === 0 && charEnd === undefined
+      ? string
+      : string.slice(charStart, charEnd)
+  return replaceInvalidSurrogate(stringA)
+}
+
+const getByteEnd = function (string, byteEnd) {
+  if (byteEnd === undefined) {
+    return byteEnd
+  }
+
+  const charEnd = byteToChar(string, byteEnd, true)
+  return charEnd === string.length ? undefined : charEnd
+}
--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
-import { byteToChar } from './direction.js'
-import { replaceInvalidSurrogate } from './surrogate.js'
+import { charCodeSlice } from './char_code.js'
 import { validateInput } from './validate.js'
 
 // Like `string.slice()` but bytewise
@@ -10,20 +9,5 @@ export default function stringByteSlice(string, byteStart, byteEnd) {
     return ''
   }
 
-  const charStart = byteToChar(string, byteStart, false)
-  const charEnd = getByteEnd(string, byteEnd)
-  const stringA =
-    charStart === 0 && charEnd === undefined
-      ? string
-      : string.slice(charStart, charEnd)
-  return replaceInvalidSurrogate(stringA)
-}
-
-const getByteEnd = function (string, byteEnd) {
-  if (byteEnd === undefined) {
-    return byteEnd
-  }
-
-  const charEnd = byteToChar(string, byteEnd, true)
-  return charEnd === string.length ? undefined : charEnd
+  return charCodeSlice(string, byteStart, byteEnd)
 }
