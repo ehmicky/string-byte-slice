ddc05be3984e399a1bc3b538ebff212afd6e0c1d
ehmicky <ehmicky@gmail.com>
2022-12-04 20:14:12 +0100

    Update type tests

--- src/main.test-d.ts
+++ src/main.test-d.ts
@@ -1,14 +1,18 @@
-import { expectType, expectNotType, expectError } from 'tsd'
+import { expectType, expectNotType } from 'tsd'
 
 import stringByteSlice from 'string-byte-slice'
 
 expectType<string>(stringByteSlice('a', 0))
-expectError(stringByteSlice(true))
-expectError(stringByteSlice('a'))
-expectError(stringByteSlice('a', '0'))
+// @ts-expect-error
+stringByteSlice(true)
+// @ts-expect-error
+stringByteSlice('a')
+// @ts-expect-error
+stringByteSlice('a', '0')
 
 expectType<''>(stringByteSlice('', 0))
 expectNotType<''>(stringByteSlice('a', 0))
 
 stringByteSlice('a', 0, -1)
-expectError(stringByteSlice('a', '0', '0'))
+// @ts-expect-error
+stringByteSlice('a', '0', '0')
